4e9558bf5284ffa2c68ab8891edd05899ff21f51
Mendez Pinacho Miguel <56448489+iAmazxing@users.noreply.github.com>
2020-03-22 00:19:40 -0600

    Add files via upload

---
+++ Ejercicio_21_03_2020.py
@@ -0,0 +1,31 @@
+print("Ejercicio #1")
+a = int(input("Dame el tamanio"))
+print(" ")
+for i in range(a):
+    print("* "*i)
+print(" ")
+print(" ")
+
+print("Ejercicio #2")
+b = int(input("Dame el tamanio"))
+print(" ")
+for i in range(b,0,-1):
+    print("* "*i)
+print(" ")
+print(" ")
+
+print("Ejercicio #3")
+c = int(input("Dame el tamanio"))
+print(" ")
+for i in range(c):
+    print("* "*i)
+for j in range(c,0,-1):
+    print("* "*j)
+print(" ")
+print(" ")
+
+print("Ejercicio #4")
+d = int(input("Dame el tamanio"))
+print(" ")
+for i in range(d):
+    print("* "*d)
